b23a5fc9c81243c3ae0760772952c2e12da36164
bogdancupsa <bogdancupsa@yahoo.com>
2024-07-17 02:55:30 +0300

    s3_n9

--- src/services/someip/SomeipService.cpp
+++ src/services/someip/SomeipService.cpp
@@ -11,7 +11,7 @@ void serialize_message (SomeIPMessage* deserialized_msg, uint8_t* serialized_msg
     std::memcpy(serialized_msg, &(deserialized_msg->someip_header), sizeof(deserialized_msg->someip_header));
   
     std::memcpy(serialized_msg + sizeof(deserialized_msg->someip_header), deserialized_msg->someip_payload, deserialized_msg->someip_header.length);
-
+std::cout << "Message serialized" << std::endl;
 }
 
 void deserialize_message (const uint8_t* serialized_msg, SomeIPMessage* deserialized_msg)
@@ -20,7 +20,7 @@ void deserialize_message (const uint8_t* serialized_msg, SomeIPMessage* deserial
     std::memcpy(&(deserialized_msg->someip_header), serialized_msg, sizeof(deserialized_msg->someip_header));
 
     std::memcpy(deserialized_msg->someip_payload, serialized_msg + sizeof(deserialized_msg->someip_header), deserialized_msg->someip_header.length);
-
+std::cout << "Message deserialized" << std::endl;
 }
 
 void send_someip_msg (SomeIPMessage* message, const char* ip, unsigned short port) 
@@ -29,6 +29,8 @@ void send_someip_msg (SomeIPMessage* message, const char* ip, unsigned short por
     uint8_t buffer[1024];
 
     serialize_message(message, buffer);
+
+    std::cout << "Sending SOME/IP message to " << ip << ":" << port << std::endl;
     socket.send(ip, port, buffer, sizeof(buffer));
 }
 
@@ -43,6 +45,6 @@ SomeIPMessage receive_someip_msg (void)
     {
         deserialize_message(reinterpret_cast<const uint8_t*>(data.c_str()), &message);
     }
-    
+
     return message;
 }
